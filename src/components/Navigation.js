5affb8d9dea0980d7283fb4a9d752d8ae7cf81a2
mullane-ian <mullane.ian@outlook.com>
2021-06-29 12:19:25 -0700

    mobile tweaks

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -11,12 +11,12 @@ function Navigation(props) {
            </Link>
         <nav>
           <ul>
-            {/* <li>
+            <li>
             <Link className="nav-link" to="/">
-                   Home
+                   Connect
                  
                  </Link>
-            </li> */}
+            </li>
             <li>
             <Link className="nav-link" to="/Work">
                    Work
