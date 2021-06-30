1ec3fbd940eef65f3e03c93fbdf9e75cdec49202
mullane-ian <mullane.ian@outlook.com>
2021-06-30 08:34:55 -0700

    mobile tweak

--- src/components/Home.js
+++ src/components/Home.js
@@ -30,7 +30,7 @@ function Borders() {
       <Plane position={[0, -viewport.height / 2, 0]} rotation={[-Math.PI / 2, 0, 0]} />
       <Plane position={[-viewport.width / 2 - 1, 0, 0]} rotation={[0, Math.PI / 2, 0]} />
       <Plane position={[viewport.width / 2 + 1, 0, 0]} rotation={[0, -Math.PI / 2, 0]} />
-      <Plane position={[0, 0, 0]} rotation={[0, 0, 0]} />
+      {/* <Plane position={[0, 0, 0]} rotation={[0, 0, 0]} /> */}
       <Plane position={[0, 0, 12]} rotation={[0, -Math.PI, 0]} />
     </>
   )
